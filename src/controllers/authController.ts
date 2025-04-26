fb27f15d075fa7d80a680b9882b92b5206c71e95
Chukwuka <emenikechukwuka65@gmail.com>
2025-04-26 16:48:57 +0100

    initail commit

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -10,34 +10,44 @@ export const login = async (req: Request, res: Response) => {
     // To check if user exists
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(400).json({ message: 'Invalid credentials' });
+     res.status(400).json({ success: false, message: 'Invalid credentials' });
+     return;
     }
 
-    // To compare passwords
+    // To Compare password
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(400).json({ message: 'Invalid credentials' });
+      res.status(400).json({ success: false, message: 'Invalid credentials' });
+      return;
     }
 
-    // to generate JWT
+    // To generate JWT 
     const token = jwt.sign(
       { id: user._id, email: user.email },
       process.env.JWT_SECRET as string,
-      { expiresIn: '7d' }
+      { expiresIn: '20m' }
     );
 
-    // to respond with token and user info (but no password!)
-    res.status(200).json({
+    // To set the token in a cookie
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production', // To secure only in productions
+      sameSite: 'strict',
+      maxAge: 20 * 60 * 1000 
+    }).status(200).json({
+      success: true,
       message: 'Login successful',
-      token,
+      token, 
       user: {
         id: user._id,
         email: user.email,
-        fullName: user.fullName,  
+        Name: user.Name,
       }
     });
+    return;
   } catch (error) {
     console.error('Login error:', error);
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ success: false, message: 'Server error' });
+    return;
   }
 };
