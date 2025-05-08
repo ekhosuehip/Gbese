d12cb342a10e54e742c3e810c37e455208dec54b
Mr-Wilson001 <gozmork@gmail.com>
2025-05-08 14:02:03 +0100

    added user account

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { getBenefactor } from '../controllers/userController';
 import { protect } from '../middlewares/authMiddleware';
+import { getUserAccount } from "../controllers/userController";
 
 const router = express.Router();
 
@@ -8,5 +9,6 @@ router.use(protect);
 
 // Route: GET /api/v5/users/Benefactor
 router.get('/benefactor',  getBenefactor);
+router.get('/user/account', getUserAccount);
 
 export default router;
